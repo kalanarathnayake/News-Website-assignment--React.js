cb779cf4fc1fe535fe32c6a777425c66abc05af6
kalanarathnayake <kalanagayanga8@gmail.com>
2023-02-19 20:26:40 +0530

    Minor changes in UI s and some configurations

--- src/components/admin-dashboard/createnews.component.js
+++ src/components/admin-dashboard/createnews.component.js
@@ -8,6 +8,11 @@ export default function CreateNews(props) {
                 <input type="search" id="default-search" class=" justify-center block w-[500px] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search News" required />
             </div>
             <div class="">
+                <Link to="/signup" class="content-center my-1 w-fit inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4: focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Create an Admin
+                </Link>
+            </div>
+            <div class="">
                 <Link to="/create-news" class="content-center my-1 w-fit inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4: focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Create News
                 </Link>
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -22,98 +22,96 @@ function SignUpPage() {
   };
 
   return (
-    <div className="max-w-full max-h-full">
+    <div className="px-6 pt-20 lg:px-8">
       <div className="px-6 pt-6 lg:px-8">
         <nav className="flex items-center justify-between" aria-label="Global">
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a
               href="/signin"
-              className="text-sm font-semibold leading-6 text-gray-900"
+              className="text-lg font-bold leading-6 text-gray-900"
             >
               Sign In <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
       </div>
-      <div className="max-w-full max-h-full flex justify-center">
-        <div className="mt-5 md:col-span-2 md:mt-0">
-          <div>
-            <form onSubmit={handleOnSubmit}>
-              <div className="overflow-hidden shadow sm:rounded-md">
-                <div className="bg-white px-4 py-5 sm:p-6">
-                  <div className="flex justify-center">
-                    <img src={logo} className="w-3/12" alt="Company Logo" />
+      <div className="flex items-center justify-center h-full px-4 py-12 sm:px-6 lg:px-8">
+        <div className="">
+          <form onSubmit={handleOnSubmit} className="mt-8 space-y-6">
+              <div className="w-full max-w-md p-10 bg-white border-2 border-solid rounded-lg shadow-md border-grey-500">
+                <div className="flex justify-center">
+                  <img src={logo} className="w-6/12" alt="Company Logo" />
+                </div>
+                <div>
+                  <h2 className="mt-6 text-3xl font-bold tracking-tight text-center text-gray-900">
+                    Sign up as a new user
+                  </h2>
+                </div>
+                <div className="-space-y-px rounded-md shadow-sm">
+                  <div className="space-y-5">
+                    <label
+                      htmlFor="first-name"
+                      className="sr-only">
+                      First name
+                    </label>
+                    <input
+                      type="text"
+                      value={fullName}
+                      onChange={(e) => setfullName(e.target.value)}
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      placeholder="First name"
+                      className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    />
                   </div>
-                  <div>
-                    <h2 className="mt-6 mb-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                      Sign up as a new user
-                    </h2>
+                  <div className="space-y-5">
+                    <label
+                      htmlFor="email-address"
+                      className="sr-only"
+                    >
+                      Email address
+                    </label>
+                    <input
+                      type="email"
+                      name="email-address"
+                      id="email-address"
+                      value={email}
+                      onChange={(e) => setemail(e.target.value)}
+                      autoComplete="email"
+                      className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Email address"
+                      />
                   </div>
-                  <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="first-name"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        First name
-                      </label>
-                      <input
-                        type="text"
-                        value={fullName}
-                        onChange={(e) => setfullName(e.target.value)}
-                        name="first-name"
-                        id="first-name"
-                        autoComplete="given-name"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                    <div className="col-span-6 sm:col-span-4">
-                      <label
-                        htmlFor="email-address"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Email address
-                      </label>
-                      <input
-                        type="email"
-                        name="email-address"
-                        id="email-address"
-                        value={email}
-                        onChange={(e) => setemail(e.target.value)}
-                        autoComplete="email"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                    <div className="col-span-6 sm:col-span-4">
-                      <label
-                        htmlFor="email-address"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Password
-                      </label>
-                      <input
-                        type="password"
-                        name="email-address"
-                        id="email-address"
-                        autoComplete="email"
-                        value={password}
-                        onChange={(e) => setpassword(e.target.value)}
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
+                  <div className="space-y-5">
+                    <label
+                      htmlFor="email-address"
+                      className="sr-only"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="email-address"
+                      id="email-address"
+                      autoComplete="email"
+                      value={password}
+                      onChange={(e) => setpassword(e.target.value)}
+                      className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Password"
+                    />
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Save
-                  </button>
-                </div>
+                <div className="py-5">
+                <button
+                  type="submit"
+                  className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-green-700 border border-transparent rounded-md group hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  Save
+                </button>
               </div>
-            </form>
-          </div>
+              </div>
+
+          </form>
         </div>
       </div>
     </div>
